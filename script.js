b363b28d25658da78dde20bf31cf8e9f049f5f65
Sejal Lal <sejallal@umich.edu>
2019-04-18 03:56:26 -0400

    extras

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function navFunction() {
     nav.className = 'navbar';
   }
 }
+
+var accord = document.getElementsByClassName("accordion");
+var x;
+
+for (x = 0; x < accord.length; x++) {
+  accord[x].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var text = this.nextElementSibling;
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  });
+}
+
+window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("navbarid");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
