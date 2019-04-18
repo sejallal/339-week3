fe2c98dc39a7a8efc28414257facb3a316ec66b6
Sejal Lal <sejallal@umich.edu>
2019-04-18 18:12:14 -0400

    extras

--- script.js
+++ script.js
@@ -40,23 +40,23 @@ function myFunction() {
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var button = document.getElementById("button");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-window.onclick = function(event) {
-  button.onclick = function() {
-    modal.style.display = "block";
-  }
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
